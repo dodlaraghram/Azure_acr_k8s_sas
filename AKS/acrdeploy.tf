cd3965ce96ae4aa84e2fc0c4559c452f34bcb0be
dodlaraghram <113520166+dodlaraghram@users.noreply.github.com>
2024-03-20 10:19:39 +0530

    Update acrdeploy.tf
    corrected acr sku

--- AKS/acrdeploy.tf
+++ AKS/acrdeploy.tf
@@ -2,5 +2,5 @@ resource "azurerm_container_registry" "demoacrsas" {
   name                = "azacrforsas"
   resource_group_name = azurerm_resource_group.demorg.name
   location            = azurerm_resource_group.demorg.location
-  sku                 = "basic"
+  sku                 = "Basic"
 }
