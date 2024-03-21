113ea902613559ae7aca524bb926b8ae149b016a
dodlaraghram <113520166+dodlaraghram@users.noreply.github.com>
2024-03-21 19:03:21 +0530

    Update acrdeploy.tf

--- AKS/acrdeploy.tf
+++ AKS/acrdeploy.tf
@@ -5,17 +5,6 @@ resource "azurerm_container_registry" "demoacrsas" {
   sku                 = "Basic"
  # admin_enabled       = true
 
-  identity {
-      type = "UserAssigned"
-      identity_ids = [
-        azurerm_user_assigned_identity.demoacrsas.id
-      ]
-    }
-  resource "azurerm_user_assigned_identity" "demoacrsas" {
-  resource_group_name = azurerm_resource_group.demorg.name
-  location            = azurerm_resource_group.demorg.location
 
-  name = "azacrforsas"
-}
 }
   
